0c9dd8176f39574011f2abc64093caadd5be5e5e
sumanchalki <suman.abc@gmail.com>
2019-07-21 23:16:27 +0530

    Installed passport and created 2 passport strategies.

---
+++ server/services/passport.js
@@ -0,0 +1,56 @@
+const passport = require('passport');
+const User = require('../models/User');
+const JwtStrategy = require('passport-jwt').Strategy;
+const ExtractJwt = require('passport-jwt').ExtractJwt;
+const LocalStrategy = require('passport-local');
+
+let config;
+if (process.env.NODE_ENV === 'production') {
+  config = require('../config/config.prod');
+}
+else if (process.env.NODE_ENV === 'dev') {
+  config = require('../config/config.dev');
+}
+
+// Create a local strategy.
+const localStrategyOptions = { usernameField: 'email' };
+const localLogin = new LocalStrategy(localStrategyOptions, function(email, password, done) {
+  User.findOne({ email: email }, function(err, userFound) {
+    if (err) { return done(err); }
+    if (!userFound) { return done(null, false); }
+
+    userFound.comparePassword(password, function(err, isMatch) {
+      if (err) { return done(err); }
+
+      // Call done with no error and user as false (not found).
+      if (!isMatch) { return done(null, false); }
+
+      // Call done with the user found.
+      return done(null, userFound);
+    });
+  });
+});
+
+// Create a JWT strategy.
+const jwtStrategyOptions = {
+  jwtFromRequest: ExtractJwt.fromHeader('authorization'),
+  secretOrKey: config.secret
+};
+
+const jwtLogin = new JwtStrategy(jwtStrategyOptions, function(payload, done) {
+  User.findById(payload.sub, function(err, user) {
+    if (err) { return done(err, false); }
+
+    if (user) {
+      // Call done with the user found.
+      done(null, user);
+    } else {
+      // Call done with no error and user as false (not found).
+      done(null, false);
+    }
+  });
+});
+
+// Use the strategies created.
+passport.use(jwtLogin);
+passport.use(localLogin);
