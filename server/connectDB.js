da313215bc19bfec54671f79d70d9d78631e1069
sumanchalki <suman.abc@gmail.com>
2019-10-18 21:05:27 +0530

    test

--- server/connectDB.js
+++ server/connectDB.js
@@ -17,7 +17,8 @@ module.exports = () => {
     .catch(error => console.error(error.stack));
   //Get the default connection
   const db = mongoose.connection;
+  console.log(db);
 
   //Bind connection to error event (to get notification of connection errors)
-  //db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+  db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 }
